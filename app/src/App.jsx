0d5b6de48e282266833eff283238eb7ef64c2680
Nabiyevich21 <mmedia10020@gmail.com>
2024-03-05 02:50:59 +0500

    counter add

---
+++ app/src/App.jsx
@@ -0,0 +1,41 @@
+import "./App.css";
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      count:0
+    }
+  }
+// ! Add Count
+  addCount =()=>{
+    this.setState({count:this.state.count+1})
+  }
+
+  // ! remove Count
+  removeCount=()=>{
+    if(this.state.count>0){
+      this.setState({count: this.state.count-1})
+    }else{
+      this.setState({count:0})
+    }
+   
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="innerH1">Hello World✌️</h1>
+        <div className="contents">
+
+          <button onClick={this.addCount} className="btn">Click Add </button>
+
+          <h3 className="countNum">{this.state.count}</h3>
+
+          <button className="btn" onClick={this.removeCount}>Click Remove</button>
+
+        </div>
+      </div>
+    )
+  }
+}
